6e0af0e77cc391b714a84792d6c3803a1969db0e
novik <novik@LAPTOP-1P787DBT>
2023-06-15 10:28:04 +0300

    Добавьте файлы проекта.

---
+++ Diplom/Windows/Zakaz.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Diplom.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Zakaz.xaml
+    /// </summary>
+    public partial class Zakaz : Window
+    {
+        public Zakaz()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           Kreplenia kreplenia= new Kreplenia();
+           kreplenia.Show();
+           Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Potolki potolki= new Potolki();
+            potolki.Show();
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Masters masters= new Masters();
+            masters.Show();
+            Close();
+        }
+    }
+}
